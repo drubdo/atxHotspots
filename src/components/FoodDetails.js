63f67b35bbc02298a77981c7a5da7d194145b951
david rubio <rubdo@davids-Air.attlocal.net>
2020-10-04 21:37:03 -0500

    added food Details, added height to images, added overflow to details, added axios

--- src/components/FoodDetails.js
+++ src/components/FoodDetails.js
@@ -1,18 +1,71 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Card from 'react-bootstrap/Card'
-
+import axios from 'axios'
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 function FoodDetails(props) {
-    
-    const viewDetails = () => {
-console.log(props, 'props')
-    };
+    const [data, setData] = useState([])
+    useEffect(() => {
+        const fetchData = async function fetchData() {
+            let res = await axios({
+                "method": "GET",
+                "url": "https://tripadvisor1.p.rapidapi.com/restaurants/list",
+                "headers": {
+                    "content-type": "application/octet-stream",
+                    "x-rapidapi-host": "tripadvisor1.p.rapidapi.com",
+                    "x-rapidapi-key": "49db63e008msha31d9ce23661601p1a123ajsnf699b90accc9",
+                    "useQueryString": true
+                }, "params": {
+                    "restaurant_tagcategory_standalone": "10591",
+                    "lunit": "km",
+                    "restaurant_tagcategory": "10591",
+                    "limit": "30",
+                    "currency": "USD",
+                    "lang": "en_US",
+                    "location_id": "30196"
+                }
+            })
 
-  return (
-    <div>
-      Details
-    </div>
-  );
+
+            console.log('data', res.data.data)
+            setData(res.data.data)
+        }
+        fetchData()
+    }, [])
+
+    return (
+        <div>
+            Details
+            <Container>
+                <Row className="justify-content-md-center">
+                    {data.map((item, index) => {
+                        return (
+                            <>
+                            {
+                                
+                                item.name && <Col lg={6} key={index}>
+                                    <Card style={{ minHeight: "200px", maxHeight: "200px", marginBottom: "10px", overflow: "hidden" }}>
+                                        <Card.Img variant="top" />
+                                        <Card.Body>
+                                            <Card.Title>{item.name}</Card.Title>
+                                            {item.photo && <img src={item.photo.images.original.url} style={{ width: "100px" }} />}
+                                            <Card.Text>
+                                                {item.description}
+                                            </Card.Text>
+                                        </Card.Body>
+                                    </Card>
+                                </Col>
+                                
+                            }
+                            </>
+                        )
+                    })}
+                </Row>
+            </Container>
+        </div>
+    );
 }
 
 export default FoodDetails;
